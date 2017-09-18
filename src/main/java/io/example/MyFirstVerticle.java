7c96092cd0c26761ff28bd5f95f0322cb028034a
pine <916931772@qq.com>
2017-09-18 23:44:13 +0800

    build

---
+++ src/main/java/io/example/MyFirstVerticle.java
@@ -0,0 +1,14 @@
+package io.example;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MyFirstVerticle extends AbstractVerticle {
+
+    public void start() {
+        vertx.createHttpServer().requestHandler(req -> {
+            req.response()
+                    .putHeader("content-type", "text/plain")
+                    .end("Hello World!");
+        }).listen(8080);
+    }
+}
