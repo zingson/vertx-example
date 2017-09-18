55a1207a2f11dae65bc11f719228661c85b55ec3
pine <916931772@qq.com>
2017-09-19 00:55:51 +0800

    build

--- src/main/java/io/example/Main.java
+++ src/main/java/io/example/Main.java
@@ -1,11 +1,12 @@
 package io.example;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 
 public class Main {
 
     public static void main(String[] args){
-        Vertx vertx = Vertx.vertx();
+        Vertx vertx = Vertx.vertx(new VertxOptions().setWorkerPoolSize(40));
 
         vertx.deployVerticle(MyFirstVerticle.class.getName());
     }
--- src/main/java/io/example/MyFirstVerticle.java
+++ src/main/java/io/example/MyFirstVerticle.java
@@ -1,14 +1,25 @@
 package io.example;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.http.HttpServerRequest;
 
 public class MyFirstVerticle extends AbstractVerticle {
 
     public void start() {
-        vertx.createHttpServer().requestHandler(req -> {
+        vertx.createHttpServer(new HttpServerOptions().setLogActivity(true)).requestHandler(req -> {
+
             req.response()
                     .putHeader("content-type", "text/plain")
                     .end("Hello World!");
         }).listen(8080);
+
+        vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
+            @Override
+            public void handle(HttpServerRequest event) {
+
+            }
+        }).listen(8980);
     }
 }
